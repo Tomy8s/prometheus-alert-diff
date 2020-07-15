be52a63f1a6bbf45e070a0be36a4d6382180158d
TomY8s <Thomas.Yates@bglgroup.co.uk>
2020-07-15 11:24:18 +0100

    added code and README.md

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# prometheus_alert_diff
+# prometheus-alert-diff
 Compare differences in alerts on different Prometheus servers
+
+## Installation
+
+Run the following command to install
+
+```bash
+$ npm install --global prometheus-alert-diff
+```
+
+The package requires a list of prometheus servers to check against. It will check the `PROMETHEUS_SERVERS` environment variable for a space-separated list. To add this on UNIX-like OSes:
+
+If you wanted to add: `https://prometheus.example.com` and `https://prometheus-test.example.com`, you would run:
+
+```bash
+$ echo 'export PROMETHEUS_SERVERS="https://prometheus.example.com https://prometheus-test.example.com`' >> ~/.bash_profile
+$ source ~/.bash_profile
+```
+
+## Running
+
+If you would like to check and compare the alert `test_alert`, run the following command:
+
+```bash
+$ prometheus-alert-diff test_alert
+```
+
+The program will search for the alert on each of the servers included in `PROMETHEUS_SERVERS` and return a list of the alerts found. If no alerts are found on a given server it will display `No matching alerts found on <server>.`
+
+## Todo
+
+- accept servers from CLI argument
+- run against multiple rules provided on CLI
+- highlight differences between alerts
+- tests
---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+const https = require('https');
+
+main()
+
+async function main() {
+    const servers = process.env.PROMETHEUS_SERVERS.split(' ');
+    if (!servers) {
+        console.error('No servers provided! Exiting...');
+        process.exit(1);
+    }
+    const alertName = process.argv[2];
+    if (!alertName) {
+        console.error('No alert name provided! Exiting...');
+        process.exit(1);
+    }
+    servers.forEach(getResult);
+
+    function getResult(server) {
+        https.get(`${server}/api/v1/rules?type=alerts`, handler);
+
+        function handler(response) {
+            let body = '';
+            response.on('data', dechunker)
+            response.on('end', print);
+
+            function print() {
+                try {
+                    const { status, data } = JSON.parse(body.replace(/([\+-]Inf)/g, '"$1"'));
+                    if (status === 'success') {
+                        const { rules } = data.groups[0];
+                        const alerts = rules.filter(alert => alert.name === alertName);
+                        if (alerts.length) {
+                            console.log(`${server}:`);
+                            console.log(alerts.map(extractLabels).join('\n'));
+                            console.log('======================================================');
+                        } else {
+                            console.log(`No matching alerts found on ${server}.`)
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error calling ' + server);
+                    console.error(error);
+                }
+            }
+
+            function dechunker(chunk) {
+                body += chunk.toString();
+            }
+        }
+    }
+
+}
+
+function extractLabels({ name, query, duration, labels, annotations }) {
+    const message = `
+name: ${ name}
+query: ${ query}
+duration: ${ duration}
+labels: ${ JSON.stringify(labels)}
+annotations: ${ generateAnnotationMsg(annotations)}
+`;
+
+    return message;
+}
+
+function generateAnnotationMsg(annotations) {
+    const lines = []
+
+    for (const annotation in annotations) {
+        lines.push(`${annotation}: ${annotations[annotation]}`);
+    }
+
+    const message = `\n  ${lines.join('\n  ')}`
+
+    return message;
+}
